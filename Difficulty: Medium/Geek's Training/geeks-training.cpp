2f3dc860cbd7eee879bf6279897d745842168576
chandan04042003 <chandanu04042003@gmail.com>
2024-12-31 00:20:01 +0530

    Update geeks-training.cpp

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -39,13 +39,45 @@ class Solution {
   // Store the result in dp array and return it
   return dp[day][last] = maxi;
 }
+
+    
   public:
-    int maximumPoints(vector<vector<int>>& arr, int n) {
-        // Code here
+    int maximumPoints(vector<vector<int>>& points, int n) {
         // Create a memoization table (dp) to store intermediate results
-      vector<vector<int>> dp(n, vector<int>(4, -1));
-      // Start the recursive calculation from the last day with no previous activity
-      return f(n - 1, 3, arr, dp);
+    //   vector<vector<int>> dp(n, vector<int>(4, -1));
+    //   // Start the recursive calculation from the last day with no previous activity
+    //   return f(n - 1, 3, points, dp);
+    
+    // TABULATION
+    // Create a 2D DP (Dynamic Programming) table to store the maximum points
+      // dp[i][j] represents the maximum points at day i, considering the last activity as j
+      vector<vector<int>> dp(n, vector<int>(4, 0));
+    
+      // Initialize the DP table for the first day (day 0)
+      dp[0][0] = max(points[0][1], points[0][2]);
+      dp[0][1] = max(points[0][0], points[0][2]);
+      dp[0][2] = max(points[0][0], points[0][1]);
+      dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+    
+      // Iterate through the days starting from day 1
+      for (int day = 1; day < n; day++) {
+        for (int last = 0; last < 4; last++) {
+          dp[day][last] = 0;
+          // Iterate through the tasks for the current day
+          for (int task = 0; task <= 2; task++) {
+            if (task != last) {
+              // Calculate the points for the current activity and add it to the
+              // maximum points obtained on the previous day (recursively calculated)
+              int activity = points[day][task] + dp[day - 1][task];
+              // Update the maximum points for the current day and last activity
+              dp[day][last] = max(dp[day][last], activity);
+            }
+          }
+    }
+  }
+
+  // The maximum points for the last day with any activity can be found in dp[n-1][3]
+  return dp[n - 1][3];
     }
 };
 
