c9a554c53b1838799da2945613abc78d0afc61f1
chandan04042003 <chandanu04042003@gmail.com>
2025-02-24 11:41:36 +0530

    Time: 0 ms (100%), Space: 10 MB (46.38%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        /* because we need the subarrays in 
+        the order --> bit masking */
+        // 2^n = 1<<n
+        int n= nums.size();
+        int subsets= 1<<n;
+        vector<vector<int>> ans;
+
+        for(int i=0;i<subsets;i++){
+             vector<int> arr;
+             for(int j=0;j<n;j++){
+                if(i & (1<<j)) arr.push_back(nums[j]);
+             }
+             ans.push_back(arr);
+        }
+        return ans;
+    }
+};
