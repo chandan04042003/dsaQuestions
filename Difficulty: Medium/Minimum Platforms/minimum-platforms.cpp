19633d6c8901b8a0f8c7f709c08c209b263cd0e8
chandan04042003 <chandanu04042003@gmail.com>
2025-04-08 17:56:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        // minimum no. of groups of non-overlapping intervals
+        vector<pair<int,char>> cont;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            cont.push_back({arr[i],'a'});
+            cont.push_back({dep[i],'d'});
+        }
+        sort(cont.begin(),cont.end());
+        int cnt=0;
+        int maxi=1;
+        for(int i=0;i<cont.size();i++){
+            if(cont[i].second=='a') cnt++;
+            else cnt--;
+            maxi=max(maxi,cnt);
+        }
+        return maxi;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, dep;
+        string input;
+
+        // Read first array (arr)
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Read second array (dep)
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            dep.push_back(number);
+        }
+
+        // Assuming Solution class is defined elsewhere with a method findPlatform
+        Solution ob;
+        cout << ob.findPlatform(arr, dep);
+
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
