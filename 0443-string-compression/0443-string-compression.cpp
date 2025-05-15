390a85f358851cef6cdf73ae73327a897c374277
chandan04042003 <chandanu04042003@gmail.com>
2025-05-15 14:32:28 +0530

    Time: 0 ms (100%), Space: 13.6 MB (88.48%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -7,18 +7,16 @@ public:
         int j = 1;     
         int k = 0;     
 
-        while (j <= chars.size()) {
-            if (j == chars.size() || chars[i] != chars[j]) {
-                chars[k++] = chars[i];
-                int count = j - i;
-                if (count > 1) {
-                    string cntStr = to_string(count);
-                    for (char c : cntStr) {
-                        chars[k++] = c;
-                    }
+        while (j<=chars.size()) {
+            while(j<chars.size() && chars[j]==chars[i]) j++;
+            chars[k++]=chars[i];
+            if(j-i>1){
+                string cnt=to_string(j-i);
+                for(char x:cnt){
+                    chars[k++]=x;
                 }
-                i = j;  
             }
+            i=j;
             j++;
         }
         return k;
