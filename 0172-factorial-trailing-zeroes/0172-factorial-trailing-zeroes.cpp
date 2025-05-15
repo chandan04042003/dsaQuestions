273a19c7d8bcd7cd7f457e847e0cccb9bd1f9087
chandan04042003 <chandanu04042003@gmail.com>
2025-05-15 20:31:22 +0530

    Time: 0 ms (100%), Space: 8.1 MB (52.04%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     int trailingZeroes(int n) {
         int cnt = 0;
         while (n >= 5) {
-            n /= 5;
+            n /= 5;// (for every iternation n/5/5=> n/25...)
             cnt += n;
         }
         return cnt;
