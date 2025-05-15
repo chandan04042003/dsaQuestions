ef20df776f0a429264f279150a1c5769ba850c81
chandan04042003 <chandanu04042003@gmail.com>
2025-05-15 07:47:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.52%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -10,7 +10,7 @@ private:
 public:
     double myPow(double x, int n) {
         long long p=n;
-        if(n<0) p=-p;
+        if(n<0) p=-p; // note:- mod(INT_MIN)>mod(INT_MAX) 
         double ans=fn(x,p);
         if(n<0) return (1/ans);
         return ans;
