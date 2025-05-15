b8d436936046c07caa2fdaa31459e2dffb71aded
chandan04042003 <chandanu04042003@gmail.com>
2025-05-15 07:46:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.67%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    double fn(double x, long long n){
+        if(n==1) return x;
+        if(n==0) return 1;
+        double a=myPow(x,n/2);
+        if(n%2==0) return (a*a);
+        else return (a*a*x);
+    }
+public:
+    double myPow(double x, int n) {
+        long long p=n;
+        if(n<0) p=-p;
+        double ans=fn(x,p);
+        if(n<0) return (1/ans);
+        return ans;
+    }
+};
