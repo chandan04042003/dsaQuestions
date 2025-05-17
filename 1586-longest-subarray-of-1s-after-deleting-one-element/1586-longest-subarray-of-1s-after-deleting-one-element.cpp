9c2872da383cdea1c5505783328381e3e681e177
chandan04042003 <chandanu04042003@gmail.com>
2025-05-17 16:24:28 +0530

    Time: 0 ms (100%), Space: 60 MB (85.17%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -6,11 +6,12 @@ public:
         int j=0;
         int maxi=0;
         while(j<nums.size()){
+            // at any nums[j]-->0/1
             if(nums[j]==0) zero++;
             while(zero>1){
                 if(nums[i]==0) zero--;
                 i++;
-            }
+            } // sliding window
             maxi=max(maxi,j-i);
             j++;
         }
