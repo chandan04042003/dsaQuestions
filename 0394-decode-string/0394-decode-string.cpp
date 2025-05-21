4abad8f0907cf8f7032fca729c2bd38c646c1a12
chandan04042003 <chandanu04042003@gmail.com>
2025-05-21 11:30:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (99.24%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -3,6 +3,7 @@ public:
     string decodeString(string s){
         stack<char> st;
         for(char x:s){
+            // for each char in s ---> alphabet / number(can be more than one digit) / [ / ]
             if(x==']'){
                 string temp;
                 while(st.top()!='['){
@@ -18,11 +19,11 @@ public:
                 }
                 reverse(num.begin(),num.end());
                 int n=stoi(num);
-                while (n--) {
+                while (n--) { // push back to stack to deal with nested loop (imp edge case)
                     for (char ch : temp) {
                         st.push(ch);
                     }
-                }
+                } 
             }
             else st.push(x);
         }
