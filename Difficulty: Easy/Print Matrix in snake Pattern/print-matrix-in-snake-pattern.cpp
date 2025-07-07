09356f17f122a4a64240451a8b498be8f71baa61
chandan04042003 <chandanu04042003@gmail.com>
2025-07-07 12:09:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Matrix in snake Pattern/print-matrix-in-snake-pattern.cpp
@@ -0,0 +1,23 @@
+
+
+class Solution {
+  public:
+    // Function to return list of integers visited in snake pattern in matrix.
+    vector<int> snakePattern(vector<vector<int> > matrix) {
+        int n=matrix.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                for(int j=0;j<n;j++){
+                    ans.push_back(matrix[i][j]);
+                }  
+            }
+            else{
+                for(int j=n-1;j>=0;j--){
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
